d94a7329028d40899683c5f86ac2b1efbe6a7b0f
deanoandrejevic <deanoandrejevic@gmail.com>
2021-08-13 15:40:50 +0000

    updates surplus worksheets

--- run.py
+++ run.py
@@ -69,6 +69,14 @@ def update_sales_worksheet(data):
 
     print("Sales worksheet updates successfully.\n")
 
+def update_surplus_worksheet(data):
+    """
+    Update surplus worksheet, as new row with the calculated data from sales and stock
+    """
+    print("Updating surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)
+
 def calculate_surplus_data(sales_row):
     """
     Compare sales with stock and calculate the surplus for each item type.
@@ -99,7 +107,7 @@ def main():
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
-    print(new_surplus_data)
+    update_surplus_worksheet(new_surplus_data)
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
