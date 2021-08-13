e6db7387948841a8ff2ebf3d5199f4ea92a36562
deanoandrejevic <deanoandrejevic@gmail.com>
2021-08-13 14:44:46 +0000

    Add user request loop to repeat request for data until data provided is valid

--- run.py
+++ run.py
@@ -5,26 +5,35 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPEAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPEAD_CLIENT.open("love_sandwiches")
 
+
 def get_sales_data():
     """
     Get sales figures input from the user
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, seperated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here:")
-    print(f"The data provided is {data_str}")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, seperated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        data_str = input("Enter your data here:")
+         
+        sales_data = data_str.split(",")
+        
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+
+    return sales_data
+
 
 def validate_data(values):
     """
@@ -32,19 +41,20 @@ def validate_data(values):
     Raise ValueError if strings cannot be converted into int,
     or if there aren't excatly 6 values.
     """
-    
+
     try:
         [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
-        f"Excatly 6 values required, you provided {len(values)}"
-        )
+                f"Excatly 6 values required, you provided {len(values)}"
+            )
     except ValueError as e:
         print(f"Invalid data {e}, please try again")
+        return False
 
-    print(values)
+    return True
 
-get_sales_data()
+    
 
 
-
+data = get_sales_data()
